59e18516ab318045feb39238acbdaac5c9e1a254
martinhopereira2 <martinhopereira.epf@hotmail.com>
2023-10-10 11:56:29 -0300

    criados atritutos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+        def __init__(self, nome=None, idade=25):
+                self.idade = idade
+                self.nome=nome
         def cumprimentar(self):
             return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-        p = Pessoa ()
+        p = Pessoa ('Martinho')
         print(Pessoa.cumprimentar(p))
         print(id(p))
         print (p.cumprimentar())
+        print(p.nome)
+        p.nome = 'Pereira'
+        print(p.nome)
+        print(p.idade)
